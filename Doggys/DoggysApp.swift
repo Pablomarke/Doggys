1048b39b57aed50c24bc1efcf1f014495a59dccd
Pablomarke <pablomarquezartesano@gmail.com>
2024-03-21 14:48:46 +0100

    First Commit with app

---
+++ Doggys/DoggysApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggysApp.swift
+//  Doggys
+//
+//  Created by Pablo Márquez Marín on 21/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DoggysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
