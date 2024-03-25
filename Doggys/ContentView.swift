eeda9f370562ae99552a3636d9a52c39269e0086
marcomadv <imarcoma@gmail.com>
2024-03-25 12:17:11 +0100

    Create simple loginView

--- Doggys/ContentView.swift
+++ Doggys/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.customLightGreen.ignoresSafeArea()
+            VStack(spacing: 5) {
+                TextField("Email", text: $email)
+                    .padding()
+                    .frame(width:280)
+                    .foregroundColor(.customBlue)
+                    .background(Color.customLightBlue)
+                    .cornerRadius(20)
+                    .shadow(radius: 10, x: 5, y: 10)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+                    .opacity(0.9)
+                    .padding(.top, 290)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .frame(width: 280)
+                    .foregroundColor(.customBlue)
+                    .background(Color.customLightBlue)
+                    .cornerRadius(20)
+                    .shadow(radius: 10, x: 5, y: 10)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+                    .opacity(0.9)
+                    .padding()
+                
+                
+                Button(action: {
+                    
+                }, label: {
+                    
+                    Text("Login")
+                        .font(.title2)
+                        .foregroundStyle(.white)
+                        .frame(width: 150, height: 40)
+                        .background(Color.customGreen)
+                        .cornerRadius(15)
+                        .shadow(radius: 15, x: 0, y: 10)
+                })
+                .padding(.top, 40)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Register")
+                        .foregroundStyle(.white)
+                        .font(.title3)
+                })
+                .padding(.top, 180)
+            }
+            
+            
         }
-        .padding()
     }
 }
 
