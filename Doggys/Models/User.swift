6ed87e2c5c6340c58f36e77941f4209e211ef8cf
marcomadv <imarcoma@gmail.com>
2024-03-27 18:40:41 +0100

    create model user

--- Doggys/Models/User.swift
+++ Doggys/Models/User.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class User {
+    let id: String
+    let email: String
+    let password: String
+    
+    init(id: String, email: String, password: String) {
+        self.id = id
+        self.email = email
+        self.password = password
+    }
+    
+    convenience init() {
+        self.init(id: "", email: "", password: "")
+    }
+}
