3ee7769df7f56d026dae11d2317caecbceefe3d3
Pablomarke <pablomarquezartesano@gmail.com>
2024-04-02 23:26:19 +0200

    Create view model and navigation to register(Change to login)

--- Doggys/UI/Splash/SplashView.swift
+++ Doggys/UI/Splash/SplashView.swift
@@ -8,21 +8,47 @@
 import SwiftUI
 
 struct SplashView: View {
+    @ObservedObject var viewModel: SplashViewModel
+    static var viewName: String = "SplashView"
+    @State private var isActive = false
+    
+    public init(viewModel: SplashViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
-        ZStack {
-            Color.customGreenblue
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                Spacer()
-                LogoView().background(Color.customGreenblue)
-                Spacer()
-                ProgressView()                    .scaleEffect(3.0)
-                Spacer()
+        NavigationStack {
+            ZStack {
+                Color.customGreenblue
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Spacer()
+                    LogoView().background(Color.customGreenblue)
+                    Spacer()
+                    ProgressView()                    .scaleEffect(3.0)
+                    Spacer()
+                }
             }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.isActive = true
+                }
+            }
+            .background(
+                NavigationLink(
+                    destination: RegisterView(),
+                    isActive: $isActive
+                ) {
+                    EmptyView()
+                }
+            )
         }
+    }
+    mutating func set(viewModel: SplashViewModel) {
+        self.viewModel = viewModel
     }
 }
 
 #Preview {
-    SplashView()
+    SplashView(viewModel: SplashViewModel())
 }
---
+++ Doggys/UI/Splash/SplashViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SplashViewModel.swift
+//  Doggys
+//
+//  Created by Pablo Márquez Marín on 2/4/24.
+//
+
+import Foundation
+
+final class SplashViewModel: ObservableObject {
+    
+}
