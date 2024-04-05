68fdc5141e03664ed3041ebc16ee4163de1bde76
marcomadv <imarcoma@gmail.com>
2024-04-05 20:07:31 +0200

    create branch profile + files

---
+++ Doggys/UI/Profile/ProfileView.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileView.swift
+//  Doggys
+//
+//  Created by Marco Muñoz on 5/4/24.
+//
+
+import SwiftUI
+
+
+struct ProfileView: View {
+    var body: some View {
+        ZStack{
+            Color.customLightBlue.ignoresSafeArea()
+            VStack{
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ Doggys/UI/Profile/ProfileWireFrame.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileWireFrame.swift
+//  Doggys
+//
+//  Created by Marco Muñoz on 5/4/24.
+//
+
+import SwiftUI
+
+final class ProfileWireFrame {
+    
+    
+}
