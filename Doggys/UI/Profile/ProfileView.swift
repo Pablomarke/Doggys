28551d570f075eef16ab24bb766e7e22d7474ed5
marcomadv <imarcoma@gmail.com>
2024-04-05 20:53:58 +0200

    test picker with dog breed in profileview

--- Doggys/UI/Profile/ProfileView.swift
+++ Doggys/UI/Profile/ProfileView.swift
@@ -9,11 +9,23 @@ import SwiftUI
 
 
 struct ProfileView: View {
+    
+    //TODO: Enum
+    let dogBreed = ["Mestizo","Dobermán","Labrador","Rottweiler","Siba-Inu","Yorkshire","Bulldog", "Teckel"]
+    
+    @State private var selectedBreed = 0
+    
     var body: some View {
         ZStack{
             Color.customLightBlue.ignoresSafeArea()
             VStack{
-                
+                Picker("Selecione una raza", selection: $selectedBreed) {
+                    ForEach(0..<dogBreed.count) {
+                        Text(self.dogBreed[$0])
+                    }
+                }
+                .pickerStyle(InlinePickerStyle())
+                .padding()
             }
         }
     }
