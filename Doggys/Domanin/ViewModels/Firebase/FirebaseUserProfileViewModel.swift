f11f3c9c74e4888daa84af3acd3e4ebb15666858
marcomadv <imarcoma@gmail.com>
2024-04-13 19:44:18 +0200

    Add firebaseUserprofileviewModel

--- Doggys/Domanin/ViewModels/Firebase/FirebaseUserProfileViewModel.swift
+++ Doggys/Domanin/ViewModels/Firebase/FirebaseUserProfileViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by Marco Muñoz on 12/4/24.
 //
 
-import Foundation
+import FirebaseFirestore
+
+class FirebaseUserProfileViewModel: UserProfileProtocol {
+    
+    private let db = Firestore.firestore()
+    private let collectionName = "userprofile_doggys"
+    
+    func fetchData(onSucces: @escaping ([UserProfile]) -> Void, onFailure: @escaping (Error) -> Void) {
+        db.collection(collectionName)
+            .addSnapshotListener { querySnapshot, error in
+                if let error = error {
+                    onFailure(error)
+                } else if let querySnapshot = querySnapshot {
+                    var data: [UserProfile] = []
+                   onSucces(data)
+                }
+            }
+    }
+    
+    func searchData(userProfile: UserProfile, onSuccess: @escaping () -> Void, onFailure: @escaping (Error) -> Void) {
+        db.collection(collectionName)
+            .addDocument(data: userProfile.dictionary) { error in
+                if let error = error {
+                    onFailure(error)
+                } else {
+                    onSuccess()
+                }
+            }
+    }
+    
+    
+}
