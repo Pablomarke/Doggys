607c829f2eed63384728365a2b7fddfec63f2cb8
Pablomarke <pablomarquezartesano@gmail.com>
2024-04-18 13:37:47 +0200

    formatted splash viewmodel

--- Doggys/UI/Splash/SplashViewModel.swift
+++ Doggys/UI/Splash/SplashViewModel.swift
@@ -13,7 +13,7 @@ final class SplashViewModel: ObservableObject {
     @Published var navigateToHome = false
     private var authViewModel: AuthProtocol
     private var logViewModel: LogProtocol
-   
+    
     init(authViewModel: AuthProtocol, logViewModel: LogProtocol) {
         self.authViewModel = authViewModel
         self.logViewModel = logViewModel
@@ -39,18 +39,18 @@ private extension SplashViewModel {
     func checkIfUserIsLoggedIn() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) { [weak self] in
             self?.authViewModel.isUserLoggedIn(
-            onSuccess: { [weak self] loggedIn in
+                onSuccess: { [weak self] loggedIn in
                     self?.navigateToHome = loggedIn
-                if !loggedIn {
-                    self?.navigateToLogin = true
+                    if !loggedIn {
+                        self?.navigateToLogin = true
+                    }
+                    
+                },
+                onFailure: { [weak self] error in
+                    self?.logViewModel.crash(screen: LoginView.viewName,
+                                             exception: error)
                 }
-                
-            },
-            onFailure: { [weak self] error in
-                 self?.logViewModel.crash(screen: LoginView.viewName,
-                                       exception: error)
-            }
-        )
-    }
+            )
+        }
     }
 }
