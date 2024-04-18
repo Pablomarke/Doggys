d3af5142c8d95d3792dab565bf1f87e7c59f11cd
marcomadv <imarcoma@gmail.com>
2024-04-18 11:58:23 +0200

    padding button save

--- Doggys/UI/Profile/ProfileView.swift
+++ Doggys/UI/Profile/ProfileView.swift
@@ -76,6 +76,7 @@ struct ProfileView: View {
                     }, label: {
                         ButtonLabel(word: "Guardar")
                     })
+                    .padding()
                 }
                 .sheet(isPresented: $isShowingImagePicker, content: {
                     ImagePicker(image: $viewModel.selectedImage)
